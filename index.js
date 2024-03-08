bc869566d853b866879465575b03c2a0c7b79664
saoxygen <104069898+saoxygen@users.noreply.github.com>
2024-03-08 19:43:18 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var bars = document.getElementById("bars");
+var menu = document.getElementById("menu2");
+
+bars.addEventListener("click", function () {
+    if (menu.className === "motion") {
+        menu.classList.remove("motion")
+    } else {
+        menu.classList.add("motion")
+    }
+})
