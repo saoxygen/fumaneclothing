92f0e27c480fcd05593c2a8eab77546d6d05ed23
saoxygen <104069898+saoxygen@users.noreply.github.com>
2024-03-09 09:44:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var bars = document.getElementById("bars");
 var menu = document.getElementById("menu2");
+var menu2_ul = document.getElementById("menu2_ul");
 
 bars.addEventListener("click", function () {
     if (menu.className === "motion") {
         menu.classList.remove("motion")
+        menu2_ul.classList.add("blur");
     } else {
         menu.classList.add("motion")
+        menu2_ul.classList.remove("blur");
     }
 })
