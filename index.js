d798280b45e6beebf035dbf61a1ae3f0023fa097
saoxygen <104069898+saoxygen@users.noreply.github.com>
2024-03-09 12:09:41 +0200

    Blur js update
    Adding blur via js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-//nothing
+var bars = document.getElementById("bars");
+var menu = document.getElementById("menu2");
+var menu2_ul = document.getElementById("menu2_ul");
+
+bars.addEventListener("click", function () {
+    if (menu.className === "motion") {
+        menu.classList.remove("motion")
+        menu2_ul.classList.add("blur");
+    } else {
+        //comments
+        menu.classList.add("motion")
+        menu2_ul.classList.remove("blur");
+    }
+})
